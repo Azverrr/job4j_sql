8b53a229dce4b75ee468dcd2aa7040614c2568cb
Azverrr <105382625+Azverrr@users.noreply.github.com>
2026-07-08 19:33:45 +0500

    Add files via upload

---
+++ scripts/505603_insert/1_insert_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(name, email)
+VALUES ('Ivan Petrov', 'ivan.petrov@mail.com')
+RETURNING *;
---
+++ scripts/505603_insert/5_insert_into_orders.sql
@@ -0,0 +1,4 @@
+INSERT INTO orders(user_id, status)
+VALUES (1, 'NEW'),
+       (2, 'PAID')
+RETURNING id, user_id, status, created_at;
