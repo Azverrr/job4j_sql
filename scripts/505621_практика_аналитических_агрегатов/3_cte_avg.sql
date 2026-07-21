26bbf8d3271289541a70fb8ea6c7c2912933a70c
Andrey Zverev <andreyzverev1987@gmail.ru>
2026-07-21 10:18:39 +0500

    Практика аналитических агрегатов

--- scripts/505621_практика_аналитических_агрегатов/3_cte_avg.sql
+++ scripts/505621_практика_аналитических_агрегатов/3_cte_avg.sql
@@ -1,7 +1,7 @@
-WITH ItemPerOrder AS (SELECT oi.order_id AS o_id, sum(oi.quantity) AS quantity_total
+WITH item_per_order AS (SELECT oi.order_id AS o_id, sum(oi.quantity) AS quantity_total
                       FROM order_items AS oi
                                JOIN orders AS o ON oi.order_id = o.order_id
                       WHERE o.status = 'completed'
                       GROUP BY oi.order_id)
 SELECT avg(quantity_total) AS avg_items_per_order
-FROM ItemPerOrder
+FROM item_per_order
